cc3cf212ad3c986c1fc791abac987f3637ecc88b
TarekiX <70344213+TarekiX@users.noreply.github.com>
2020-12-17 11:23:50 -0600

    Add files via upload

---
+++ Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Tarek Elbendary on 10/16/20.
+//  Copyright © 2020 Tarek Elbendary. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
---
+++ Colors with Navigation/Colors with Navigation/ColorsWithNaviViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ColorsWithNaviViewController.swift
+//  Colors with Navigation
+//
+//  Created by Tarek Elbendary on 10/16/20.
+//  Copyright © 2020 Tarek Elbendary. All rights reserved.
+//
+
+import UIKit
+
+class ColorsWithNaviViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown)]
+
+    @IBOutlet weak var colorsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Colors"
+
+        // Do any additional setup after loading the view.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ColorDetailViewController,
+            let row = colorsTableView.indexPathForSelectedRow?.row
+        {
+            destination.color = colors[row]
+        }
+    }
+
+}
